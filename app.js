e408b77e1fdd9d8578a5632795972d4ce79f1ed6
Rawb!t <bunnyrawbit@outlook.com>
2018-04-27 13:05:06 +0100

    Electron window manager

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 'use strict';
 
 const electron = require('electron')
-const {app, BrowserWindow} = electron
+//const {app, BrowserWindow} = electron
+const app = electron.app
+const windowManager = require('electron-window-manager')
 
 var mainWindow = null;
 
 app.on('ready', function() {
-    mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800
+    
+    windowManager.init({
+        'appBase': 'main',
+        'layouts': {
+            'default': 'main/layouts/control.html',
+            'display': 'main/layouts/display.html'
+        },
+        'defaultLayout': 'default',
+        'defaultWindowTitle': 'GifJ',
+        'onLoadFailure': function(window){
+            window.loadURL('/error.html');
+        }
+    })
+    windowManager.open('main', 'GifJ', '/index.html')
+    
+    var displayWindow = windowManager.createNew('display', 'Display', '/index.html', false, {
+        'width': 200,
+        'height': 200,
+        'position': 'topLeft',
+        'layout': 'display',
+        'resizable': true
     });
-
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    
+     windowManager.open(displayWindow)
 });
