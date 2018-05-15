029b2ef2e5418bb89950a3a10ef19ff5535c0911
Rawb!t <bunnyrawbit@outlook.com>
2018-05-15 22:03:12 +0100

    Request

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const env = require('node-env-file')
 const electron = require('electron')
 const app = electron.app
 
+const ipcMain = require('electron').ipcMain;
+
 const windowManager = require('electron-window-manager')
 const giphyAPI = process.env.GIFYAPI
 env(__dirname + '/.env')
 
-const giphy = require('giphy-api')(giphyAPI)
+const request = require('request')
 
 app.on('ready', function () {
 
@@ -28,7 +30,8 @@ app.on('ready', function () {
         'width': 1000,
         'height': 750,
         'position': 'topLeft',
-        'resizable': true
+        'resizable': true,
+        showDevTools: true
     })
 
     var display = windowManager.open('display', 'Display', false, false, {
@@ -37,12 +40,30 @@ app.on('ready', function () {
         'position': 'bottomRight',
         'layout': 'display',
         'resizable': true,
-        'menu': null
+        'menu': null,
+        showDevTools: false
     })
 
-   /* giphy.search('cat').then(function (res) {
-        var gifURL = res.data[0].embed_url
-        //display.loadURL(gifURL)
-        display.html("<div class='container__display' style='background-image: url('" + gifURL + "');'></div>")
-    })*/
+    main.onReady(true, function (window) {
+        windowManager.bridge.emit('test', 'test message', display, main)
+
+    })
+
+    windowManager.bridge.on('test', function (event) {
+        display.html('<h3>' + event + '</h3>')
+    })
+
+    ipcMain.on('search', function (event, data) {
+        console.log(giphyAPI)
+        if (!data == "") {
+            let reqUrl = 'http://api.giphy.com/v1/gifs/search?q=' + data + '&api_key=' + giphyAPI
+            request(reqUrl, function (error, response, body) {
+                console.log('error:', error); // Print the error if one occurred
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                console.log('body:', body); // Print the HTML for the Google homepage.
+            });
+
+        }
+    })
+
 })
